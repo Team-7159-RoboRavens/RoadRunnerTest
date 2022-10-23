27a2fc255824d16f79fd296fcae58cb4388b07a9
k-Jandhyala <60835325+k-Jandhyala@users.noreply.github.com>
2022-10-23 13:18:23 -0700

    Basic Tank has been refactored

--- FtcRobotController/src/main/java/Team7159/BasicRobots/BasicTank.java
+++ FtcRobotController/src/main/java/Team7159/BasicRobots/BasicTank.java
@@ -3,6 +3,8 @@ package Team7159.BasicRobots;
 import com.arcrobotics.ftclib.hardware.motors.*;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.hardware.motors.*;
+import com.qualcomm.robotcore.hardware.*;
 
 import Team7159.Enums.Direction;
 import Team7159.Enums.Version;
@@ -23,13 +25,13 @@ public class BasicTank {
     // This is because we can either do a four wheel drive or a two wheel drive
     //
     */
-    public Motor RFMotor;
-    public Motor RBMotor;
-    public Motor LFMotor;
-    public Motor LBMotor;
+    public DcMotor RFMotor;
+    public DcMotor RBMotor;
+    public DcMotor LFMotor;
+    public DcMotor LBMotor;
 
-    public Motor LMotor;
-    public Motor RMotor;
+    public DcMotor LMotor;
+    public DcMotor RMotor;
 
     int drive;
 
@@ -37,23 +39,23 @@ public class BasicTank {
 
         drive = 4;
 
-        LFMotor = new Motor(Map, "FLDrive");
-        LBMotor = new Motor(Map, "BLDrive");
-        RFMotor = new Motor(Map, "FRDrive");
-        RBMotor = new Motor(Map, "BRDrive");
+        LFMotor = Map.dcMotor.get("FLDrive");
+        LBMotor = Map.dcMotor.get("BLDrive");
+        RFMotor = Map.dcMotor.get("FRDrive");
+        RBMotor = Map.dcMotor.get("BRDrive");
 
-        LFMotor.setRunMode(Motor.RunMode.RawPower);
-        RFMotor.setRunMode(Motor.RunMode.RawPower);
-        LBMotor.setRunMode(Motor.RunMode.RawPower);
-        RBMotor.setRunMode(Motor.RunMode.RawPower);
+        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        RFMotor.set(0.0);
-        RBMotor.set(0.0);
-        LFMotor.set(0.0);
-        LBMotor.set(0.0);
+        LFMotor.setPower(0.0);
+        LBMotor.setPower(0.0);
+        RFMotor.setPower(0.0);
+        RBMotor.setPower(0.0);
 
-        LFMotor.setInverted(true);
-        LBMotor.setInverted(true);
+        LFMotor.setDirection(DcMotor.Direction.REVERSE);
+        LBMotor.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
@@ -61,68 +63,68 @@ public class BasicTank {
 
         drive = 2;
 
-        LMotor = new Motor(Map, "LDrive");
-        RMotor = new Motor(Map, "RDrive");
+        LMotor = Map.dcMotor.get("LDrive");
+        RMotor = Map.dcMotor.get("RDrive");
 
-        LMotor.setRunMode(Motor.RunMode.RawPower);
-        RMotor.setRunMode(Motor.RunMode.RawPower);
+        LMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        LMotor.set(0);
-        RMotor.set(0);
+        LMotor.setPower(0);
+        RMotor.setPower(0);
 
-        RMotor.setInverted(true);
+        RMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
 //    This stuff is all deprecated by the FTCLib update, not sure if we're ever gonna refactor this
-//    public void moveStraight(double power){
-//        Left.setPowers(power);
-//        Right.setPowers(power);
-//    }
-//
-//    public void stop(){
-//        Left.stop();
-//        Right.stop();
-//    }
-//
-//    public void turn(Direction direction, int degrees){
-//        Right.resetEncoders();
-//        Left.resetEncoders();
-//        int LeftDistance = Comp.computeTurningPos(direction, degrees, Direction.LEFT, 26.5, Version.TWO);
-//        int RightDistance = Comp.computeTurningPos(direction, degrees, Direction.RIGHT, 26.5, Version.TWO);
-//        Left.setTargetPosition(LeftDistance);
-//        Right.setTargetPosition(RightDistance);
-//        Left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        Right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        moveStraight(0.5);
-//        while(Left.isBusy()&&Right.isBusy()){}
-//        stop();
-//        Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//    }
-//
-//    public void driveDir(Direction direction, double distance){
-//        Right.resetEncoders();
-//        Left.resetEncoders();
-//        Right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        Left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        switch(direction){
-//            case FORWARDS:
-//                int pos = -Comp.computePositionD(RobotMath.toMeters(distance), Version.TWO);
-//                Right.setTargetPosition(pos);
-//                Left.setTargetPosition(pos);
-//                break;
-//            case BACKWARDS:
-//                pos = Comp.computePositionD(RobotMath.toMeters(distance), Version.TWO);
-//                Right.setTargetPosition(pos);
-//                Left.setTargetPosition(pos);
-//                break;
-//        }
-//        moveStraight(0.5);
-//        while(Right.isBusy()&&Left.isBusy()){}
-//        stop();
-//        Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//    }
+    public void moveStraight(double power){
+        Left.setPowers(power);
+        Right.setPowers(power);
+    }
+
+    public void stop(){
+        Left.stop();
+        Right.stop();
+    }
+
+    public void turn(Direction direction, int degrees){
+        Right.resetEncoders();
+        Left.resetEncoders();
+        int LeftDistance = Comp.computeTurningPos(direction, degrees, Direction.LEFT, 26.5, Version.TWO);
+        int RightDistance = Comp.computeTurningPos(direction, degrees, Direction.RIGHT, 26.5, Version.TWO);
+        Left.setTargetPosition(LeftDistance);
+        Right.setTargetPosition(RightDistance);
+        Left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        moveStraight(0.5);
+        while(Left.isBusy()&&Right.isBusy()){}
+        stop();
+        Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void driveDir(Direction direction, double distance){
+        Right.resetEncoders();
+        Left.resetEncoders();
+        Right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        switch(direction){
+            case FORWARDS:
+                int pos = -Comp.computePositionD(RobotMath.toMeters(distance), Version.TWO);
+                Right.setTargetPosition(pos);
+                Left.setTargetPosition(pos);
+                break;
+            case BACKWARDS:
+                pos = Comp.computePositionD(RobotMath.toMeters(distance), Version.TWO);
+                Right.setTargetPosition(pos);
+                Left.setTargetPosition(pos);
+                break;
+        }
+        moveStraight(0.5);
+        while(Right.isBusy()&&Left.isBusy()){}
+        stop();
+        Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 
 }
